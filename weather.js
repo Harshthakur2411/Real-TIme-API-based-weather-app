bb40f27d2201e147c0dcb4e5c25d5180e77a263a
Harshitthakur2411 <rjharshthakur1103@gmail.com>
2025-07-22 12:52:53 +0530

    wether-app commit

---
+++ weather.js
@@ -0,0 +1,36 @@
+import readline from "readline/promises";
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+const API_KEY = "9b044e83e6708319823024d39416efd8";
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+const getweather = async (city)=>{
+    const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`;
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`City not Found !!!!`);
+
+        }
+        const weatherData = await response.json();
+        console.log(
+         ` 🌍  ${weatherData.name} 
+         | 🌡️  ${weatherData.main.temp}°C 
+         | 🌤️  ${weatherData.weather[0].description} 
+         | 💧  ${weatherData.main.humidity}% 
+         | 🌬️  ${weatherData.wind.speed} m/s`
+         );
+
+        
+    } catch (err) {
+        console.log(err);
+        
+    }
+
+}
+
+const city = await rl.question("Enter the city name: ");
+await getweather(city);
+rl.close();
